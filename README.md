7c589852d89d974e2d3a71c59aca120272ba642c
Soumen Choudhury <soumenchoudhury.mca@gmail.com>
2021-03-28 03:32:39 -0400

    Added req, res, next param and create shop-item.json to use it next

--- README.md
+++ README.md
@@ -36,3 +36,10 @@ TDD approach to write Unit/ Integartion using Jest/ Supertest for Express REST A
 4. shopping-list.controller.js: invoke ShopItemModel.create() inside the addItem() - observe test is passing
 5. Observe Mongoose "console.warn", follow the step mentioned in the url to enable node test environement. create jest.config.js in the root folder and add the mentioned content.
 6. re-run the test and confirm the warning is gone
+
+# s6-setup-node-mocks-http-to-mock-req-res-next
+1. npm install node-mocks-http --save-dev
+2. shopping-list.controller.test.js: setup node-mocks-http and mock req, res
+3. Create ./test/mock-data/shop-item.json mock data file and use it in the test.
+4. Modify the test to pass req, res, next in ShoppingListController.addItem(req, res, next) 
+5. Observe the test is failing, modify shopping-list.controller.js to accept req, res, next params
--- test/unit/shopping-list.controller.test.js
+++ test/unit/shopping-list.controller.test.js
@@ -1,6 +1,9 @@
 const { it, expect } = require("@jest/globals");
+const httpMocks = require("node-mocks-http");
+
 const ShoppingListController = require("../../api/controller/shopping-list.controller");
 const ShopItemModel = require("../../database/models/ShopItem");
+const newShopItem = require("../mock-data/shop-item.json");
 
 ShopItemModel.create = jest.fn();
 
@@ -9,7 +12,13 @@ describe("ShoppingListController.addItem", () => {
         expect(typeof ShoppingListController.addItem).toBe("function");
     })
     it("should call ShopItemModel.create", () => {
-        ShoppingListController.addItem();
+        let req, res, next;
+        req = httpMocks.createRequest();
+        res = httpMocks.createResponse();
+        next= null;
+        req.body = newShopItem;
+
+        ShoppingListController.addItem(req, res, next);
         expect(ShopItemModel.create).toBeCalled();
     })
 })
